27dd7cc35b440d14e03e8b42e203c2aab936e4b9
Jorge <grind.xaero@gmail.com>
2024-03-03 02:53:35 +0100

    Añadir CORS

--- backend/api/index.php
+++ backend/api/index.php
@@ -12,6 +12,14 @@ require 'src/application/LoginController.php';
 $app = new \Slim\App(['settings' => $config]);
 $container = setContainer($app->getContainer());
 
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 $app->get('/users', function (Request $request, Response $response, array $args) {
     $user_service = $this->get('userService');
 
@@ -107,6 +115,11 @@ $app->post('/login', function (Request $request, Response $response, array $args
     }
 
     return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler;
+    return $handler($req, $res);
 });
 
 $app->run();
